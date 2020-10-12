4597dd70ddcf1570d24a8db43e1b3c3d5ec5d3c4
Lydia Vandenbergh Jackson <45639445+lvanden@users.noreply.github.com>
2020-10-11 20:31:49 -0400

    added additional attributes to model

--- words/migrations/0001_initial.py
+++ words/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('name', models.CharField(max_length=255)),
+                ('part_of_speech', models.TextField()),
                 ('definition', models.TextField()),
+                ('example', models.TextField()),
             ],
         ),
     ]
--- words/models.py
+++ words/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Word(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255)
+    part_of_speech = models.TextField(default='')
     definition = models.TextField()
+    example = models.TextField(default='')
     
     def __str__(self):
         return self.name
